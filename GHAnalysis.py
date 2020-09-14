2b4ec3e5d9b179174c4c203fc08bf6b5a15196bf
HL1123 <1306295598@qq.com>
2020-09-14 22:28:33 +0800

    多进程

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -1,6 +1,8 @@
 import json
 import os
 import argparse
+from multiprocessing import Pool
+
 
 class Data:
     def __init__(self, dict_address: int = None, reload: int = 0):
@@ -14,90 +16,103 @@ class Data:
         self.__4Events4PerR = json.loads(x)
         x = open('3.json', 'r', encoding='utf-8').read()
         self.__4Events4PerPPerR = json.loads(x)
+#处理文件 把数据处理后的list覆盖原json的内容 解决子进程向父进程传递数据问题
+    def merge(self, f, dict_address):
+        json_list = []
+        if f[-5:] == '.json':
+            json_path = f
+            x = open(dict_address+'\\'+json_path,
+                    'r', encoding='utf-8').read()
+            str_list = [_x for _x in x.split('\n') if len(_x) > 0]
+            for i, _str in enumerate(str_list):
+                try:
+                    json_list.append(json.loads(_str))
+                except:
+                    pass
+        records = []
+        records = self.__listOfNestedDict2ListOfDict(json_list)
+        with open(dict_address+'\\'+f,'w') as r:            
+            json.dump(records,r)
 
     def __init(self, dict_address: str):
-        self.__4Events4PerP = {} 
+        self.__4Events4PerP = {}
         self.__4Events4PerR = {}
         self.__4Events4PerPPerR = {}
-#将文件读出到list和字符串处理合并在一起，即读出一行处理一行，这样list就不会太大以至于内存爆炸。
-#下次优化可能是往时间上优化。
+        # 将文件读出到list和字符串处理合并在一起 即读出一行处理一行 这样list就不会太大以至于内存爆炸。
+        pool = Pool(processes=5)
         for root, dic, files in os.walk(dict_address):
             for f in files:
-                json_list = []
-                if f[-5:] == '.json':
-                    json_path = f
-                    x = open(dict_address+'\\'+json_path,
-                             'r', encoding='utf-8').read()
-                    str_list = [_x for _x in x.split('\n') if len(_x) > 0]
-                    for i, _str in enumerate(str_list):
-                        try:
-                            json_list.append(json.loads(_str))
-                        except:
-                            pass
-                records = self.__listOfNestedDict2ListOfDict(json_list)
+                # 开启多进程读入 然后再把处理好的list覆盖读入的文件 优化时间
+                pool.apply_async(func=self.merge, args=(f, dict_address))
+            pool.close()            
+            pool.join()
+            records=[]
+            for f in files:
+                with open( dict_address +'\\'+f,'r') as x:
+                    records=json.load(x)
                 for i in records:
                     if not self.__4Events4PerP.get(i['actor__login'], 0):
                         self.__4Events4PerP.update({i['actor__login']: {}})
                         self.__4Events4PerPPerR.update({i['actor__login']: {}})
                     self.__4Events4PerP[i['actor__login']][i['type']
-                                                ] = self.__4Events4PerP[i['actor__login']].get(i['type'], 0)+1
+                                              ]=self.__4Events4PerP[i['actor__login']].get(i['type'], 0)+1
                     if not self.__4Events4PerR.get(i['repo__name'], 0):
                         self.__4Events4PerR.update({i['repo__name']: {}})
                     self.__4Events4PerR[i['repo__name']][i['type']
-                                            ] = self.__4Events4PerR[i['repo__name']].get(i['type'], 0)+1
+                                         ]=self.__4Events4PerR[i['repo__name']].get(i['type'], 0)+1
                     if not self.__4Events4PerPPerR[i['actor__login']].get(i['repo__name'], 0):
                         self.__4Events4PerPPerR[i['actor__login']].update({i['repo__name']: {}})
                     self.__4Events4PerPPerR[i['actor__login']][i['repo__name']][i['type']
-                                                          ] = self.__4Events4PerPPerR[i['actor__login']][i['repo__name']].get(i['type'], 0)+1
+                        ]=self.__4Events4PerPPerR[i['actor__login']][i['repo__name']].get(i['type'], 0)+1
         with open('1.json', 'w', encoding='utf-8') as f:
-            json.dump(self.__4Events4PerP,f)
+            json.dump(self.__4Events4PerP, f)
         with open('2.json', 'w', encoding='utf-8') as f:
-            json.dump(self.__4Events4PerR,f)
+            json.dump(self.__4Events4PerR, f)
         with open('3.json', 'w', encoding='utf-8') as f:
-            json.dump(self.__4Events4PerPPerR,f)
+            json.dump(self.__4Events4PerPPerR, f)
 
     def __parseDict(self, d: dict, prefix: str):
-        _d = {}
+        _d={}
         for k in d.keys():
             if str(type(d[k]))[-6:-2] == 'dict':
                 _d.update(self.__parseDict(d[k], k))
             else:
-                _k = f'{prefix}__{k}' if prefix != '' else k
-                _d[_k] = d[k]
+                _k=f'{prefix}__{k}' if prefix != '' else k
+                _d[_k]=d[k]
         return _d
 
     def __listOfNestedDict2ListOfDict(self, a: list):
-        records = []
+        records=[]
         for d in a:
-            _d = self.__parseDict(d, '')
+            _d=self.__parseDict(d, '')
             records.append(_d)
         return records
 
     def getEventsUsers(self, username: str, event: str) -> int:
-        if not self.__4Events4PerP.get(username,0):
+        if not self.__4Events4PerP.get(username, 0):
             return 0
         else:
-            return self.__4Events4PerP[username].get(event,0)
+            return self.__4Events4PerP[username].get(event, 0)
 
     def getEventsRepos(self, reponame: str, event: str) -> int:
-        if not self.__4Events4PerR.get(reponame,0):
+        if not self.__4Events4PerR.get(reponame, 0):
             return 0
         else:
-            return self.__4Events4PerR[reponame].get(event,0)
+            return self.__4Events4PerR[reponame].get(event, 0)
 
     def getEventsUsersAndRepos(self, username: str, reponame: str, event: str) -> int:
-        if not self.__4Events4PerP.get(username,0):
+        if not self.__4Events4PerP.get(username, 0):
             return 0
-        elif not self.__4Events4PerPPerR[username].get(reponame,0):
+        elif not self.__4Events4PerPPerR[username].get(reponame, 0):
             return 0
         else:
-            return self.__4Events4PerPPerR[username][reponame].get(event,0)
+            return self.__4Events4PerPPerR[username][reponame].get(event, 0)
 
 
 class Run:
     def __init__(self):
-        self.parser = argparse.ArgumentParser()
-        self.data = None
+        self.parser=argparse.ArgumentParser()
+        self.data=None
         self.argInit()
         print(self.analyse())
 
@@ -106,24 +121,24 @@ class Run:
         self.parser.add_argument('-u', '--user')
         self.parser.add_argument('-r', '--repo')
         self.parser.add_argument('-e', '--event')
-        
+
     def analyse(self):
         if self.parser.parse_args().init:
-            self.data = Data(self.parser.parse_args().init, 1)
+            self.data=Data(self.parser.parse_args().init, 1)
             return 0
         else:
             if self.data is None:
-                self.data = Data()
+                self.data=Data()
             if self.parser.parse_args().event:
                 if self.parser.parse_args().user:
                     if self.parser.parse_args().repo:
-                        res = self.data.getEventsUsersAndRepos(
+                        res=self.data.getEventsUsersAndRepos(
                             self.parser.parse_args().user, self.parser.parse_args().repo, self.parser.parse_args().event)
                     else:
-                        res = self.data.getEventsUsers(
+                        res=self.data.getEventsUsers(
                             self.parser.parse_args().user, self.parser.parse_args().event)
                 elif self.parser.parse_args().repo:
-                    res = self.data.getEventsRepos(
+                    res=self.data.getEventsRepos(
                         self.parser.parse_args().repo, self.parser.parse_args().event)
                 else:
                     raise RuntimeError('error: argument -l or -c are required')
@@ -133,4 +148,4 @@ class Run:
 
 
 if __name__ == '__main__':
-    a = Run()
+    a=Run()
